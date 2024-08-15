4103355fd8ced62e7eb1a889475fc2dfe0883c4c
Syn <76777947+Philotus@users.noreply.github.com>
2024-08-15 11:04:27 +0300

    new bad push

--- CalcApp/CalculatorApp/Calculator.cs
+++ CalcApp/CalculatorApp/Calculator.cs
@@ -15,7 +15,7 @@ namespace CalcApp.CalculatorApp
 
         public int Subtract(int a, int b)
         {
-            return a + b;
+            return a * b;
         }
 
         public int Multiply(int a, int b)
